d86c98505e6127145c231f9128a4e6f443fd5c54
theemadnes <alex.mattson@gmail.com>
2019-12-25 00:12:19 -0600

    matched sleep loop delay with consumption loop

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import argparse
 
+# set up logging 
 out_hdlr = logging.StreamHandler(sys.stdout)
 fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 out_hdlr.setFormatter(fmt)
@@ -10,6 +11,7 @@ out_hdlr.setLevel(logging.INFO)
 logging.getLogger().addHandler(out_hdlr)
 logging.getLogger().setLevel(logging.INFO)
 
+# define command line arguments 
 parser = argparse.ArgumentParser(description='Chomp (consume) some memory.')
 parser.add_argument('--megabytes', '-m', dest='megabytes', type=int, help="How many megabytes should this run chomp?", default='1000')
 parser.add_argument('--delay', '-d', dest='delay', type=int, help="How many seconds should the chomping delay after every 100MB?", default='1')
@@ -20,7 +22,8 @@ logging.info("Chomping %i megabytes with a delay of %i seconds per 100MB..." % (
 
 chomp_dict = {}
 
-for i in range(0,args.megabytes):
+# memory consumption loop
+for i in range(0, args.megabytes):
     
     chomp_dict[i] = bytearray(1024000)
 
@@ -30,7 +33,9 @@ for i in range(0,args.megabytes):
         
         time.sleep(args.delay)
 
+# done chomping, now just log & sleep
 while True:
 
-    logging.info("Done chomping %i megabytes... Now sleeping every 20s" % (args.megabytes))
-    time.sleep(20)
+    logging.info("Done chomping %i megabytes... Now sleeping every %is" % (args.megabytes, args.delay))
+    
+    time.sleep(args.delay)
