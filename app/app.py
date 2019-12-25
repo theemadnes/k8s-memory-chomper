7937375ba6fbb865983c5565a83c8f641e45eb3c
theemadnes <alex.mattson@gmail.com>
2019-12-24 23:49:35 -0600

    fixed types

--- app/app.py
+++ app/app.py
@@ -11,28 +11,26 @@ logging.getLogger().addHandler(out_hdlr)
 logging.getLogger().setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser(description='Chomp (consume) some memory.')
-parser.add_argument('--megabytes', help="How many megabytes should this run chomp?")
+parser.add_argument('--megabytes', '-m', dest='megabytes', type=int, help="How many megabytes should this run chomp?", default='1000')
+parser.add_argument('--delay', '-d', dest='delay', type=int, help="How many seconds should the chomping delay after every 100MB?", default='1')
 
 args = parser.parse_args()
 
-# handle unset param and just eat 256Gbytes
-if not args.megabytes:
-    args.megabytes = 256000
-
-logging.info("Chomping %s megabytes..." % (args.megabytes)) 
+logging.info("Chomping %i megabytes with a delay of %i seconds per 100MB..." % (args.megabytes, args.delay)) 
 
 chomp_dict = {}
 
-for i in range(0,int(args.megabytes)):
+for i in range(0,args.megabytes):
     
     chomp_dict[i] = bytearray(1024000)
 
     if not (i+1)%100:
 
         logging.info("Chomped %i megabytes" % (i+1))
-        time.sleep(1)
+        
+        time.sleep(args.delay)
 
 while True:
 
-    logging.info("Done chomping... Now sleeping every 20s")
+    logging.info("Done chomping %i megabytes... Now sleeping every 20s" % (args.megabytes))
     time.sleep(20)
